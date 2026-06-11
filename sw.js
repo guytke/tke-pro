69e9ace865290a29882a2a824a642afa2932547c
guy <guyshtaiger@gmail.com>
2026-06-11 09:47:40 +0300

    Add PWA manifest, service worker and app icon

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'tke-pro-v1';
+const ASSETS = ['/', '/tke-pro/', '/tke-pro/index.html', '/tke-pro/icon.jpg', '/tke-pro/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      var clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
